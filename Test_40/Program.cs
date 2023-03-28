9b13f6661df8124c1d61dcac8ea61a3684c5ffc3
Evgenia <evgga@mail.ru>
2023-03-28 19:51:08 +0900

    На вход программы подаются три целых положительных числа. Определить, является ли это сторонами треугольника. Если да, то вывести его площадь

---
+++ Test_40/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задача 40. На вход программы подаются три целых положительных числа. 
+//Определить , является ли это сторонами треугольника. Если да, то вывести его площадь.
+
+Console.Write("Введите строну а: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите строну b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите строну c: ");
+double c = double.Parse(Console.ReadLine());
+
+if ((a + b > c) && (b + c > a) && (a + c > b))
+{
+    double p = (a + b + c) / 2;
+    Console.WriteLine("Это треугольник");
+    Console.WriteLine("Площадь треугольника: " + Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+}
+else
+{
+    Console.WriteLine("Это не треугольник");
+}
+if (a + b <= c || a + c <= b || b + c <= a)
+{
+    Console.WriteLine("Треугольник не существует.\n");
+}
+
+Console.ReadKey();
