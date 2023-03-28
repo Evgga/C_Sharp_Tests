767f86fedf9cc555d1248c07244148bdf1ca7aba
Evgenia <evgga@mail.ru>
2023-03-28 17:55:19 +0900

    Программа, которая принимает на вход целое число любой разрядности и на выходе показывет вторую цифру слева этого числа

---
+++ Test_10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10. Напишите программу, ктр принимает на вход целое число любой разрядности и 
+//на выходе показывает вторую цифру слева этого числа.
+
+Console.Write("Введите число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
